6498ecab87d908f644ecf0b7002a3d245f08045e
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-25 21:44:11 +0700

    fix: pass focus state into coupled gaze

--- src/main.jsx
+++ src/main.jsx
@@ -383,6 +383,7 @@ function Scene({
           interaction={interaction}
           interactionEnabled={interactionEnabled}
           ritual={ritual}
+          focusActive={focusActive}
         />
       </Suspense>
       <ContactShadows
