9538c97b10b0c31a67bed632b0f63941d937a5a1
Loccao102 <138382005+Loccao102@users.noreply.github.com>
2026-09-25 10:57:17 +0700

    fix: correct VeyraSignature import newline

--- src/VeyraModel.jsx
+++ src/VeyraModel.jsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
-import * as THREE from 'three'\nimport VeyraSignature from './VeyraSignature'
+import * as THREE from 'three'
+import VeyraSignature from './VeyraSignature'
 
 const STATE = {
   idle:      { bob: .08, sway: .035, halo: .55, core: 1.0, orbit: .25, ear: .04, eye: 1.0 },
